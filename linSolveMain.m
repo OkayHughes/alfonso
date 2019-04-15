a97183ea6ed00eb0125bd01a4f00cbdce5f9594f
Owen Hughes <okayhughes@gmail.com>
2019-04-14 23:11:41 -0400

    Make iterative refinement the norm

--- linSolveMain.m
+++ linSolveMain.m
@@ -23,34 +23,32 @@ function dsoln = linSolveMain(soln, probData, RHS, myLinSolve, algParams, opts)
     [m, n] = size(probData.A);
     delta  = myLinSolve(soln, probData, RHS);
     if opts.maxItRefineSteps > 0 
-        % checks to see if we need to refine the solution
-        if rcond(full(soln.H)) < eps            
+        % checks to see if we need to refine the solution       
             
-            LHS                         = probData.LHS;    
-            LHS(m+n+1+(1:n),m+(1:n))    = soln.mu*soln.H;
-            LHS(end,m+n+1)              = soln.mu/soln.tau^2;
-            
-            exitFlag    = 0;
-            res         = residual3p(LHS, delta, RHS);
-            resNorm     = norm(res);  
-            for iter = 1:opts.maxItRefineSteps
-                fprintf("ITERATIVE REFINEMENT STEP\n")
-                if exitFlag; break; end;
-                d           = myLinSolve(soln, probData, res);
-                deltaNew	= delta - d;
-                resNew      = residual3p(LHS, deltaNew, RHS);
-                resNewNorm	= norm(resNew);
-                    
-                % stops iterative refinement if there is not enough progress
-                if resNewNorm > algParams.itRefineThreshold*resNorm
-                    exitFlag = 1;
-                end
-                % updates solution if residual norm is smaller
-                if resNewNorm < resNorm
-                    delta       = deltaNew;
-                    res         = resNew;
-                    resNorm     = resNewNorm;
-                end
+        LHS                         = probData.LHS;    
+        LHS(m+n+1+(1:n),m+(1:n))    = soln.mu*soln.H;
+        LHS(end,m+n+1)              = soln.mu/soln.tau^2;
+
+        exitFlag    = 0;
+        res         = residual3p(LHS, delta, RHS);
+        resNorm     = norm(res);  
+        for iter = 1:opts.maxItRefineSteps
+            fprintf("ITERATIVE REFINEMENT STEP\n")
+            if exitFlag; break; end;
+            d           = myLinSolve(soln, probData, res);
+            deltaNew	= delta - d;
+            resNew      = residual3p(LHS, deltaNew, RHS);
+            resNewNorm	= norm(resNew);
+
+            % stops iterative refinement if there is not enough progress
+            if resNewNorm > algParams.itRefineThreshold*resNorm
+                exitFlag = 1;
+            end
+            % updates solution if residual norm is smaller
+            if resNewNorm < resNorm
+                delta       = deltaNew;
+                res         = resNew;
+                resNorm     = resNewNorm;
             end
         end
         
