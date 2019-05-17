abca1333d3d4411d74bd1d76b5ca6144fd037524
Owen Hughes <okayhughes@gmail.com>
2019-05-17 15:36:56 -0400

    Uses serialized FeketeBases for alfonso (i.e. reuse serialization routines)

--- testPolyOpt.m
+++ testPolyOpt.m
@@ -1,15 +1,16 @@
-function ans = testPolyOpt(frac)
-degree = 20;
+function ret = testPolyOpt(frac)
+degree = 6;
 
-vars = msspoly('x', 2);
-costPoly = vars(1)^2 + vars(2)^2 - 1;
-ub = [1; 1];
-lb = [-1; -1];
+vars = msspoly('x', 3);
+costPoly = vars(1)^5 + vars(2)^2 - vars(3)^1 + 1;
+ub = [1; 1; 1];
+lb = [-1; -1; -1];
 polyWeights = (vars - ub) .* (lb - vars);
 
-intParams = FeketeCubeRand(size(vars, 1), degree, frac)
-ans = polyOpt(vars, costPoly, polyWeights, ub, lb, intParams)
+intParams = FeketeBasis(size(vars, 1), degree, 1);
+intParams = cubeFromBasis(intParams);
+ret = polyOpt(vars, costPoly, polyWeights, ub, lb, intParams)
 
-scatter(cell2mat(ans.hessIterNum), cell2mat(ans.hessEigArea))
+scatter(cell2mat(ret.hessIterNum), cell2mat(ret.hessEigArea))
 
 end
