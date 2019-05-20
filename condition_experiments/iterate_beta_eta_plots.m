3f952377c3b10a209bafea9e83325bf4a8d03247
Owen Hughes <okayhughes@gmail.com>
2019-05-20 16:32:10 -0400

    Rename, change plot axes

--- condition_experiments/iterate_beta_eta_plots.m
+++ condition_experiments/iterate_beta_eta_plots.m
@@ -1,10 +1,10 @@
-function iterate_beta_eta(rets, labels, figname)
+function iterate_beta_eta_plots(rets, labels, figname)
 
 [iter_nums, iprods] = iterate_quantity_from_cell_array(rets, @iterate_eta);
 figure(); hold on;
 title("Distance from iterate $i$ to final iterate");
 xlabel("iterate");
-ylabel("Inner product between successive iterates")
+ylabel("\eta")
 for i=1:size(labels, 1)
     plot(iter_nums{i}, iprods{i}, "-o", 'LineStyle', 'none', 'DisplayName',labels{i})
 
